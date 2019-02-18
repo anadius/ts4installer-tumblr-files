9a98da9c113a30d184cf5055a358ac0f54cc4e9e
anadius <anadius@gmail.com>
2019-02-18 10:46:13 +0100

    fix gallery UserScript
    didn't work on item details

--- userjs/gallery.user.js
+++ userjs/gallery.user.js
@@ -3,7 +3,7 @@
 // @author      anadius
 // @match       *://www.ea.com/*/games/the-sims/the-sims-4/pc/gallery*
 // @match       *://www.ea.com/games/the-sims/the-sims-4/pc/gallery*
-// @version     1.0.3
+// @version     1.0.4
 // @grant       none
 // @namespace   anadius.github.io
 // @icon        https://anadius.github.io/ts4installer-tumblr-files/userjs/favicon.png
@@ -18,7 +18,12 @@ document.addEventListener('click', e => {
   
   if(el.tagName === 'LI' && el.classList.contains('stream-tile__actions-download')) {
     e.stopPropagation();
-    const uuid = el.parentNode.parentNode.parentNode.getAttribute('uuid');
+    el = el.parentNode.parentNode.parentNode;
+    let uuid = null;
+    while(uuid === null) {
+      uuid = el.getAttribute('uuid');
+      el = el.parentNode;
+    }
     window.open('https://ts4installer.tumblr.com/d?id=' + encodeURIComponent(uuid), '_blank');
   }
 }, true);
